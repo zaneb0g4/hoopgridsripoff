aa8d56ec8b5f5245cf8221a823a5195591bb81f8
zaneboga <25zboga@athenian.org>
2024-05-07 09:11:49 -0700

    5/7/24 update

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -10,7 +10,6 @@ public class GameBoard {
             "Portland Trail Blazers","Sacramento Kings","San Antonio Spurs","Toronto Raptors","Utah Jazz",
             "Washington Wizards"};
 
-
     JFrame frame;
 
     DrawingPanel dpanel;
@@ -24,26 +23,6 @@ public class GameBoard {
 
         frame.pack();
         frame.setVisible(true);
-
-        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-        JPanel teamRow = new JPanel();
-        frame.add(teamRow);
-
-        teamRow.setLayout(new BoxLayout(teamRow, BoxLayout.X_AXIS));
-        String team1 = allTeams[(int)(Math.random()*30)];
-        String team2 = allTeams[(int)(Math.random()*30)];
-        String team3 = allTeams[(int)(Math.random()*30)];
-        while(team1.equals(team2) || team1.equals(team3) || team2.equals(team3)){
-            team1 = allTeams[(int)(Math.random()*30)];
-            team2 = allTeams[(int)(Math.random()*30)];
-            team3 = allTeams[(int)(Math.random()*30)];
-        }
-        JLabel teamName1 = new JLabel(team1 + "      ");
-        JLabel teamName2 = new JLabel(team2 + "      ");
-        JLabel teamName3 = new JLabel(team3);
-        teamRow.add(teamName1);
-        teamRow.add(teamName2);
-        teamRow.add(teamName3);
     }
 
     public static void main(String[] args) {
@@ -61,10 +40,45 @@ public class GameBoard {
             return new Dimension(500, 500);
         }
 
-        protected void paintcomponent(Graphics g){
+        protected void paintComponent(Graphics g){
+            String team1 = allTeams[(int)(Math.random()*30)];
+            String team2 = allTeams[(int)(Math.random()*30)];
+            String team3 = allTeams[(int)(Math.random()*30)];
+            String team4 = allTeams[(int)(Math.random()*30)];
+            String team5 = allTeams[(int)(Math.random()*30)];
+            String team6 = allTeams[(int)(Math.random()*30)];
+            while(team1.equals(team2) || team1.equals(team3) || team1.equals(team4) || team1.equals(team5) ||
+            team1.equals(team6) || team2.equals(team3) || team2.equals(team4) || team2.equals(team5) ||
+            team2.equals(team6) || team3.equals(team4) || team3.equals(team5) || team3.equals(team6) ||
+            team4.equals(team5) || team4.equals(team6) || team5.equals(team6)){
+                team1 = allTeams[(int)(Math.random()*30)];
+                team2 = allTeams[(int)(Math.random()*30)];
+                team3 = allTeams[(int)(Math.random()*30)];
+                team4 = allTeams[(int)(Math.random()*30)];
+                team5 = allTeams[(int)(Math.random()*30)];
+                team6 = allTeams[(int)(Math.random()*30)];
+            }
+
             super.paintComponent(g);
 
+            g.setColor(Color.PINK);
+            g.fillOval(100, 10, 100, 80);
+            g.fillOval(225, 10, 100, 80);
+            g.fillOval(350, 10, 100, 80);
+            g.fillOval(10, 100, 80, 100);
+            g.fillOval(10, 225, 80, 100);
+            g.fillOval(10, 350, 80, 100);
+
             g.setColor(Color.BLACK);
+
+            g.drawString(team1, 105, 50);
+            g.drawString(team2, 230, 50);
+            g.drawString(team3, 355, 50);
+
+            g.drawString(team4, 15, 150);
+            g.drawString(team5, 15, 275);
+            g.drawString(team6, 15, 400);
+
             g.drawRect(100, 100, 100, 100);
             g.drawRect(225, 100, 100, 100);
             g.drawRect(350, 100, 100, 100);
